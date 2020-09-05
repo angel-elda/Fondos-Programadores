96274139ba3b2ca4f401e59e601f1e84f9ad500d
angel-elda <41248617+angel-elda@users.noreply.github.com>
2020-09-05 08:02:13 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cd /tmp
-wget https://raw.githubusercontent.com/angel-elda/Fondos-Programadores/master/links.txt
-
 if [ -d /usr/share/backgrounds/ ];
 then
 echo "Sí, sí existe." & cd /usr/share/backgrounds/
@@ -11,7 +8,5 @@ echo "No, no existe" & mkdir /usr/share/backgrounds/ & cd /usr/share/backgrounds
 fi
 
 wget -i https://raw.githubusercontent.com/angel-elda/Fondos-Programadores/master/links.txt -N
-
-rm /tmp/links.txt
 
 exit
